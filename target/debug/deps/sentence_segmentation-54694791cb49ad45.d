86af9479feec2fbdb404636275901c3df2b7e5b3
luxiant <luxiante@gmail.com>
2024-11-06 03:58:28 +0000

    changed

---
+++ target/debug/deps/sentence_segmentation-54694791cb49ad45.d
@@ -0,0 +1,7 @@
+/workspace/sentence_segmentation/target/debug/deps/libsentence_segmentation-54694791cb49ad45.rmeta: src/lib.rs
+
+/workspace/sentence_segmentation/target/debug/deps/libsentence_segmentation-54694791cb49ad45.rlib: src/lib.rs
+
+/workspace/sentence_segmentation/target/debug/deps/sentence_segmentation-54694791cb49ad45.d: src/lib.rs
+
+src/lib.rs:
